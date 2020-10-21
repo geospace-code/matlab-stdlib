b7b24def863fdeb80387874903d6a7ef8fefb0d4
Michael Hirsch <scivision@users.noreply.github.com>
2020-10-21 03:20:01 -0400

    test classes

--- +hdf5nc/TestNetCDF.m
+++ +hdf5nc/TestNetCDF.m
@@ -1,9 +1,14 @@
-function tests = test_hdf5
-tests = functiontests(localfunctions);
+classdef TestNetCDF < matlab.unittest.TestCase
+
+properties
+TestData
 end
 
-function setupOnce(tc)
-import hdf5nc.h5save
+
+methods (TestClassSetup)
+
+function setup_file(tc)
+import hdf5nc.ncsave
 
 A0 = 42.;
 A1 = [42.; 43.];
@@ -18,75 +23,73 @@ tc.TestData.A2 = A2;
 tc.TestData.A3 = A3;
 tc.TestData.A4 = A4;
 
-basic = tempname + ".h5";
+basic = tempname + ".nc";
 tc.TestData.basic = basic;
 
 % create test data first, so that parallel tests works
-h5save(basic, '/A0', A0)
-h5save(basic, '/A1', A1)
-h5save(basic, '/A2', A2)
-h5save(basic, '/A3', A3, "size", size(A3))
-h5save(basic, '/A4', A4)
+ncsave(basic, 'A0', A0)
+ncsave(basic, 'A1', A1)
+ncsave(basic, 'A2', A2, "dims", {'x2', size(A2,1), 'y2', size(A2,2)})
+ncsave(basic, 'A3', A3, "dims", {'x3', size(A3,1), 'y3', size(A3,2), 'z3', size(A3,3)})
+ncsave(basic, 'A4', A4, "dims", {'x4', size(A4,1), 'y4', size(A4,2), 'z4', size(A4,3), 'w4', size(A4,4)})
+end
 end
 
-function teardownOnce(tc)
+
+methods (TestClassTeardown)
+function cleanup(tc)
 delete(tc.TestData.basic)
 end
-
-
-function test_auto_chunk_size(tc)
-tc.verifyEqual(auto_chunk_size([1500,2500,1000,500,100]), [12,20,8,8,2])
-tc.verifyEqual(auto_chunk_size([15,250,100]), [2,32,25])
-tc.verifyEqual(auto_chunk_size([15,250]), [15,250])
 end
 
 
+methods (Test)
 function test_get_variables(tc)
-import hdf5nc.h5variables
+import hdf5nc.ncvariables
 
-vars = h5variables(tc.TestData.basic);
+vars = ncvariables(tc.TestData.basic);
 tc.verifyEqual(sort(vars), ["A0", "A1", "A2", "A3", "A4"])
 end
 
 
 function test_exists(tc)
-import hdf5nc.h5exists
+import hdf5nc.ncexists
 import matlab.unittest.constraints.IsScalar
 
-e0 = h5exists(tc.TestData.basic, '/A3');
+e0 = ncexists(tc.TestData.basic, 'A3');
 tc.verifyThat(e0, IsScalar)
 tc.verifyTrue(e0)
 
-tc.verifyFalse(h5exists(tc.TestData.basic, '/oops'))
+tc.verifyFalse(ncexists(tc.TestData.basic, '/oops'))
 
-e1 = h5exists(tc.TestData.basic, ["A3", "oops"]);
+e1 = ncexists(tc.TestData.basic, ["A3", "oops"]);
 tc.verifyTrue(isrow(e1))
 tc.verifyEqual(e1, [true, false])
 end
 
 
 function test_size(tc)
-import hdf5nc.h5size
+import hdf5nc.ncsize
 import matlab.unittest.constraints.IsScalar
 basic = tc.TestData.basic;
 
-s = h5size(basic, '/A0');
+s = ncsize(basic, 'A0');
 tc.verifyThat(s, IsScalar)
 tc.verifyEqual(s, 1)
 
-s = h5size(basic, '/A1');
+s = ncsize(basic, 'A1');
 tc.verifyThat(s, IsScalar)
 tc.verifyEqual(s, 2)
 
-s = h5size(basic, '/A2');
+s = ncsize(basic, 'A2');
 tc.verifyTrue(isvector(s))
 tc.verifyEqual(s, [4,4])
 
-s = h5size(basic, '/A3');
+s = ncsize(basic, 'A3');
 tc.verifyTrue(isvector(s))
 tc.verifyEqual(s, [4,3,2])
 
-s = h5size(basic, '/A4');
+s = ncsize(basic, 'A4');
 tc.verifyTrue(isvector(s))
 tc.verifyEqual(s, [4,3,2,5])
 end
@@ -98,36 +101,36 @@ import matlab.unittest.constraints.IsFile
 basic = tc.TestData.basic;
 tc.assumeThat(basic, IsFile)
 
-s = h5read(basic, '/A0');
+s = ncread(basic, '/A0');
 tc.verifyThat(s, IsScalar)
 tc.verifyEqual(s, 42)
 
-s = h5read(basic, '/A1');
+s = ncread(basic, '/A1');
 tc.verifyTrue(isvector(s))
 tc.verifyEqual(s, tc.TestData.A1)
 
-s = h5read(basic, '/A2');
+s = ncread(basic, '/A2');
 tc.verifyTrue(ismatrix(s))
 tc.verifyEqual(s, tc.TestData.A2)
 
-s = h5read(basic, '/A3');
+s = ncread(basic, '/A3');
 tc.verifyEqual(ndims(s), 3)
 tc.verifyEqual(s, tc.TestData.A3)
 
-s = h5read(basic, '/A4');
+s = ncread(basic, '/A4');
 tc.verifyEqual(ndims(s), 4)
 tc.verifyEqual(s, tc.TestData.A4)
 end
 
 
 function test_coerce(tc)
-import hdf5nc.h5save
+import hdf5nc.ncsave
 import matlab.unittest.constraints.IsFile
 basic = tc.TestData.basic;
 
-h5save(basic, '/int32', 0, "type", 'int32')
-h5save(basic, '/int64', 0, "type", 'int64')
-h5save(basic, '/float32', 0, "type", 'float32')
+ncsave(basic, 'int32', 0, "type", 'int32')
+ncsave(basic, 'int64', 0, "type", 'int64')
+ncsave(basic, 'float32', 0, "type", 'float32')
 
 tc.assumeThat(basic, IsFile)
 
@@ -138,30 +141,27 @@ end
 
 
 function test_rewrite(tc)
-import hdf5nc.h5save
+import hdf5nc.ncsave
 import matlab.unittest.constraints.IsFile
 basic = tc.TestData.basic;
 
-h5save(basic, '/A2', 3*magic(4))
+ncsave(basic, 'A2', 3*magic(4))
 
 tc.assumeThat(basic, IsFile)
-tc.verifyEqual(h5read(basic, '/A2'), 3*magic(4))
+tc.verifyEqual(ncread(basic, 'A2'), 3*magic(4))
 end
 
-function test_string(tc)
-import hdf5nc.h5save
-basic = tc.TestData.basic;
+function test_no_char_string(tc)
+import hdf5nc.ncsave
+tc.verifyError(@() ncsave(tc.TestData.basic, "/a_string", "hello"), 'MATLAB:validators:mustBeNumeric')
 
-h5save(basic, "/a_string", "hello")
-h5save(basic, "/a_char", 'there')
-
-astr = h5read(basic, "/a_string");
-achar = h5read(basic, "/a_char");
-tc.verifyEqual(astr, "hello")
-tc.verifyEqual(achar, "there")
 end
 
 function test_real_only(tc)
-import hdf5nc.h5save
-tc.verifyError(@() h5save(tc.TestData.basic, "/bad_imag", 1j), 'MATLAB:validators:mustBeReal')
+import hdf5nc.ncsave
+tc.verifyError(@() ncsave(tc.TestData.basic, "/bad_imag", 1j), 'MATLAB:validators:mustBeReal')
+end
+
+end
+
 end
