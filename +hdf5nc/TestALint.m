a9df517508d71c5e048f1df6e845a141e2bbebe7
Michael Hirsch <scivision@users.noreply.github.com>
2020-10-21 03:59:30 -0400

    parametrize lint

--- +hdf5nc/TestALint.m
+++ +hdf5nc/TestALint.m
@@ -4,6 +4,10 @@ properties
 TestData
 end
 
+properties (TestParameter)
+file = get_files()
+end
+
 methods (TestClassSetup)
 
 function get_files(tc)
@@ -11,43 +15,36 @@ import matlab.unittest.constraints.IsFile
 import matlab.unittest.constraints.IsFolder
 
 cwd = fileparts(mfilename('fullpath'));
-folder = fullfile(cwd, '..');
 cfg_file = fullfile(cwd, "MLint.txt");
 
-tc.assumeThat(folder, IsFolder)
 tc.assumeThat(cfg_file, IsFile)
 
-tc.TestData.folder = folder;
 tc.TestData.cfg_file = cfg_file;
 
-flist = dir(tc.TestData.folder + "/**/*.m");
-N = length(flist);
-tc.assumeGreaterThan(N, 0)
-
-tc.TestData.flist = flist;
-tc.TestData.N = N;
 end
 
 end
+
 
 methods (Test)
 
-function test_lint(tc)
+function test_lint(tc, file)
+res = checkcode(file, "-config=" + tc.TestData.cfg_file, "-fullpath");
 
-flist = tc.TestData.flist;
-
-for i = 1:tc.TestData.N
-  file = fullfile(flist(i).folder, flist(i).name);
-
-  res = checkcode(file, "-config=" + tc.TestData.cfg_file, "-fullpath");
-
-  for j = 1:length(res)
-    tc.verifyFail(append(file, ":", int2str(res(j).line), " ", res(j).message))
-  end
+for j = 1:length(res)
+  tc.verifyFail(append(file, ":", int2str(res(j).line), " ", res(j).message))
+end
 end
 
 end
 
 end
 
+
+function filenames = get_files()
+cwd = fileparts(mfilename('fullpath'));
+flist = dir(fullfile(cwd, '/**/*.m'));
+for i = 1:length(flist)
+ filenames{i} = fullfile(flist(i).folder, flist(i).name); %#ok<AGROW>
+end
 end
