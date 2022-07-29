da680b0ac07be385085cfd97485e2cc6797eb962
Michael Hirsch <scivision@users.noreply.github.com>
2022-07-29 18:00:16 -0400

    fileio.which: Windows: also work with WSL from native Windows

--- +stdlib/+fileio/which.m
+++ +stdlib/+fileio/which.m
@@ -1,28 +1,40 @@
-function exe = which(name, fpath)
+function exe = which(filename, fpath)
 % fileio.which Find executable with name under path
 % like Python shutil.which, may return relative or absolute path
 
 arguments
-  name (1,1) string {mustBeNonzeroLengthText}
+  filename (1,1) string {mustBeNonzeroLengthText}
   fpath (1,:) string = getenv('PATH')
 end
 
 import stdlib.fileio.is_exe
 import stdlib.fileio.expanduser
 
+names = filename;
+
 if ispc
-  if ~endsWith(name, ".exe")
-    name = name + ".exe";
+  % Windows executable filename doesn't necessarily need .exe,
+  % particularly for WSL executables that is_exe() will detect from
+  % native Windows Matlab.
+  if ~endsWith(filename, ".exe")
+    names(2) = filename + ".exe";
   end
 end
 
-if strlength(fileparts(name)) > 0
-  % has directory part
-  if is_exe(name)
-    exe = name;
-    return
+%% directory/filename given
+for name = names
+
+  if strlength(fileparts(name)) > 0
+    % has directory part
+    if is_exe(name)
+      exe = name;
+      return
+    end
   end
-end
+
+end % for name
+
+%% path given
 
 if isscalar(fpath)
   fpath = split(expanduser(fpath), pathsep).';
@@ -34,12 +46,16 @@ if ispc
   fpath = [pwd, fpath];
 end
 
-for p = fpath
-  exe = fullfile(p, name);
-  if is_exe(exe)
-    return
+for name = names
+
+  for p = fpath
+    exe = fullfile(p, name);
+    if is_exe(exe)
+      return
+    end
   end
-end
+
+end % for name
 
 exe = string.empty;
 
--- buildfile.m
+++ buildfile.m
@@ -2,6 +2,10 @@ function plan = buildfile
 plan = buildplan(localfunctions);
 end
 
+function lintTask(~)
+assertSuccess(runtests('stdlib.TestLint'))
+end
+
 function testTask(~)
 assertSuccess(runtests('stdlib'))
 end
