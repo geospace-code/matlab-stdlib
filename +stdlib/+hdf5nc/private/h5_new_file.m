ee36e33111614d1fd69cdb46fe3fc3866a9e755b
Michael Hirsch <scivision@users.noreply.github.com>
2022-09-28 18:52:47 -0400

    hdf5: write scalar compatible with Matlab < R2020a
    deduplicate logic for auto-size

--- +stdlib/+hdf5nc/private/h5_new_file.m
+++ +stdlib/+hdf5nc/private/h5_new_file.m
@@ -2,24 +2,27 @@ function h5_new_file(filename, varname, A, sizeA)
 
 if isempty(sizeA)
   if isscalar(A)
-    h5_write_scalar(filename, varname, A)
-    return
+    sizeA = 0;
   elseif isvector(A)
-    h5create(filename, varname, length(A), "Datatype", class(A))
+    sizeA = length(A);
   else
-    create_compress(filename, varname, A, size(A))
+    sizeA = size(A);
   end
-else
-  if isscalar(sizeA)
-    if sizeA == 0
+end
+
+if isscalar(sizeA)
+  if sizeA == 0
+    if verLessThan('matlab', '9.8')
+      h5create(filename, varname, 1, "Datatype", class(A))
+    else
       h5_write_scalar(filename, varname, A)
       return
-    else
-      h5create(filename, varname, sizeA, "Datatype", class(A))
     end
   else
-    create_compress(filename, varname, A, sizeA)
+    h5create(filename, varname, sizeA, "Datatype", class(A))
   end
+else
+  create_compress(filename, varname, A, sizeA)
 end
 
 h5write(filename, varname, A)
