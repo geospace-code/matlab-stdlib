e635358a85f2d6d673d6ebd5ca9bcbddd9f7739f
Michael Hirsch <scivision@users.noreply.github.com>
2022-10-26 17:30:49 -0400

    buildfile: add coverage task

--- buildfile.m
+++ buildfile.m
@@ -16,6 +16,30 @@ assert(~isempty(r), "No tests were run")
 assertSuccess(r)
 end
 
+function coverageTask(~)
+import matlab.unittest.TestRunner;
+import matlab.unittest.Verbosity;
+import matlab.unittest.plugins.CodeCoveragePlugin;
+import matlab.unittest.plugins.XMLPlugin;
+import matlab.unittest.plugins.codecoverage.CoberturaFormat;
+
+name = "matmap3d";
+
+suite = testsuite(name);
+
+mkdir('code-coverage');
+mkdir('test-results');
+
+runner = TestRunner.withTextOutput();
+runner.addPlugin(XMLPlugin.producingJUnitFormat('test-results/results.xml'));
+runner.addPlugin(CodeCoveragePlugin.forPackage(name, 'Producing', CoberturaFormat('code-coverage/coverage.xml')));
+
+results = runner.run(suite);
+assert(~isempty(results), "no tests found")
+
+assertSuccess(results)
+end
+
 function publishTask(~)
 % publish (generate) docs from Matlab project
 
