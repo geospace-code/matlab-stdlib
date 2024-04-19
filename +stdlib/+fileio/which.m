a28071299852610da4275abcefbc780810f532ca
scivision <scivision@users.noreply.github.com>
2024-04-19 10:33:27 -0400

    which: handle windows uppercase filename corner case

--- +stdlib/+fileio/which.m
+++ +stdlib/+fileio/which.m
@@ -1,5 +1,10 @@
 function exe = which(filename, fpath)
-%% which  like Python shutil.which, find executable in fpath or env var PATH
+%% which()
+% like Python shutil.which, find executable in fpath or env var PATH
+% does not resolve path.
+% That is, can return relative path if executable is in:
+% * (Windows) in cwd
+% * (all) fpath or Path contains relative paths
 arguments
   filename (1,1) string {mustBeNonzeroLengthText}
   fpath (1,:) string = getenv('PATH')
@@ -11,7 +16,7 @@ if ispc
   % Windows executable filename doesn't necessarily need .exe,
   % particularly for WSL executables that is_exe() will detect from
   % native Windows Matlab.
-  if ~endsWith(filename, ".exe")
+  if ~endsWith(lower(filename), ".exe")
     names(2) = filename + ".exe";
   end
 end
