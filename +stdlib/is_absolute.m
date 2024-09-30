2a4285744272989e678d2ba3ee2e5fa2346e93ab
scivision <scivision@users.noreply.github.com>
2024-09-30 10:57:43 -0400

    is_absolute: intrinsic 5x-10x faster than java

--- +stdlib/is_absolute.m
+++ +stdlib/is_absolute.m
@@ -1,12 +1,23 @@
-function isabs = is_absolute(p)
+function isabs = is_absolute(p, use_java)
 %% IS_ABSOLUTE
 % is path absolute?
 %
 % https://docs.oracle.com/en/java/javase/22/docs/api/java.base/java/io/File.html#isAbsolute()
 arguments
   p (1,1) string
+  use_java (1,1) logical = false
 end
 
-isabs = java.io.File(p).toPath().isAbsolute();
+if use_java
+  % java is about 5x to 10x slower than intrinsic
+  isabs = java.io.File(p).toPath().isAbsolute();
+else
+  L = strlength(p);
+  if ispc
+    isabs = L >= 2 && isletter(extractBetween(p, 1, 1)) && extractBetween(p, 2, 2) == ":";
+  else
+    isabs = L >= 1 && startsWith(p, "/");
+  end
+end
 
 end
