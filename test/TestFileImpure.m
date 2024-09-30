69c5bf7f0b34d0aac748d0d95129287fa70ef8b5
scivision <scivision@users.noreply.github.com>
2024-09-30 10:57:22 -0400

    TestWhich own class

--- test/TestFileImpure.m
+++ test/TestFileImpure.m
@@ -228,54 +228,6 @@ function test_samepath(tc, in_same, other_same, ref_same)
 tc.verifyEqual(stdlib.samepath(in_same, other_same), ref_same)
 end
 
-%%
-function test_which_name(tc)
-
-tc.verifyEmpty(stdlib.which(tempname))
-
-if ispc
-  n = "pwsh.exe";
-else
-  n = "ls";
-end
-%% which: Matlab in environment variable PATH
-% MacOS Matlab does not source .zshrc so Matlab is not on internal Matlab PATH
-% Unix-like OS may have Matlab as alias which is not visible to
-% stdlib.which()
-% virus scanners may block stdlib.which("cmd.exe") on Windows
-tc.verifyNotEmpty(stdlib.which(n))
-
-end
-
-
-function test_is_exe_which_fullpath(tc)
-import matlab.unittest.constraints.IsFile
-import matlab.unittest.constraints.EndsWithSubstring
-
-tc.verifyFalse(stdlib.is_exe(""))
-tc.verifyFalse(stdlib.is_exe(tempname))
-
-n = "matlab";
-%% is_exe test
-p = fullfile(matlabroot, "bin", n);
-if ispc
-  fp = p + ".exe";
-else
-  fp = p;
-end
-tc.verifyTrue(stdlib.is_exe(fp))
-%% which: test absolute path
-exe = stdlib.which(p);
-
-if ispc
-  tc.verifyThat(exe, EndsWithSubstring(".exe"))
-else
-  tc.verifyThat(exe, ~EndsWithSubstring(".exe"))
-end
-tc.verifyThat(exe, IsFile)
-
-end
-
 function test_hash(tc)
 import matlab.unittest.constraints.IsFile
 import matlab.unittest.fixtures.TemporaryFolderFixture
