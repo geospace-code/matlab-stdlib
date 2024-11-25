f1d28b3421d2dd541f2e05d7fe130286e6bff9a4
scivision <scivision@users.noreply.github.com>
2024-11-25 13:56:26 -0500

    posix: avoid duplicate coerce

--- +stdlib/posix.m
+++ +stdlib/posix.m
@@ -2,15 +2,15 @@
 % convert a path to a Posix string path separated with "/" even on Windows.
 % If Windows path also have escaping "\" this breaks
 
-function p = posix(p)
+function r = posix(p)
 arguments
   p string
 end
 
-p = string(p);
-
 if ispc
-  p = strrep(p, "\", "/");
+  r = strrep(p, "\", "/");
+else
+  r = p;
 end
 
 end
