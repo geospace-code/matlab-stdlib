e3ddd86b44ef4e363a0b2cb80cd92a2d021ca1c0
scivision <scivision@users.noreply.github.com>
2024-12-26 12:30:33 -0500

    bench normalize [skip ci]

---
+++ example/bench_normalize.m
@@ -0,0 +1,12 @@
+a1 = "c:/a///.//../////b";
+
+fno = @() stdlib.normalize(a1, false);
+fjava = @() stdlib.normalize(a1, true);
+
+t_no = timeit(fno);
+t_java = timeit(fjava);
+
+disp("No Java: " + t_no + " s")
+disp("Java: " + t_java + " s")
+
+disp("Java is " + t_no/t_java + " times faster")
