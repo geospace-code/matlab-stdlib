7c60a61d7b701ba0f57afd0848769234860de252
scivision <scivision@users.noreply.github.com>
2024-12-28 18:02:07 -0500

    test: split into files

---
+++ test/TestWithSuffix.m
@@ -0,0 +1,15 @@
+classdef TestWithSuffix < matlab.unittest.TestCase
+
+properties (TestParameter)
+p = {{"foo.h5", ".nc", "foo.nc"}, {"c", "", "c"}, {"c.nc", "", "c"}, ...
+{"", ".nc", ".nc"}, {"a//b///c///", ".h5", "a/b/c/.h5"}, ...
+{"a/b/.h5", ".nc", "a/b/.h5.nc"}, {"a/b", ".nc", "a/b.nc"}};
+end
+
+methods (Test)
+function test_with_suffix(tc, p)
+tc.verifyEqual(stdlib.with_suffix(p{1}, p{2}), p{3})
+end
+end
+
+end
