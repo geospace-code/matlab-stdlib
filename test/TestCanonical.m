c972820b172c2f4512c1af82b2443702affbb324
scivision <scivision@users.noreply.github.com>
2024-12-28 20:30:43 -0500

    testResolve: improve use_java coverage

--- test/TestCanonical.m
+++ test/TestCanonical.m
@@ -6,7 +6,9 @@ use_java = num2cell(unique([stdlib.has_java(), false]))
 p = {{"", ""}, {"not-exist", "not-exist"}, {"a/../b", "b"}, ...
 {"~", stdlib.homedir()}, {"~/", stdlib.homedir()}, ...
 {"~/..", stdlib.parent(stdlib.homedir())}, ...
-{mfilename("fullpath") + ".m/..", stdlib.parent(mfilename("fullpath"))}};
+{mfilename("fullpath") + ".m/..", stdlib.parent(mfilename("fullpath"))}, ...
+{"~/not-exist/a/..", stdlib.homedir() + "/not-exist"}
+};
 end
 
 
@@ -14,11 +16,6 @@ methods(Test)
 
 function test_canonical(tc, p, use_java)
 tc.verifyEqual(stdlib.canonical(p{1}, true, use_java), p{2})
-end
-
-function test_static(tc)
-% ~ is expanded even without expanduser when path exists
-tc.verifyEqual(stdlib.canonical("~/nobody/a/..", false), "~/nobody")
 end
 
 end
--- test/TestResolve.m
+++ test/TestResolve.m
@@ -1,8 +1,30 @@
 classdef TestResolve < matlab.unittest.TestCase
 
+properties (TestParameter)
+use_java = num2cell(unique([stdlib.has_java(), false]))
+
+p
+end
+
+methods (TestParameterDefinition, Static)
+
+function p = init()
+c = stdlib.posix(pwd());
+p = {...
+{"", c}, {"not-exist", c + "/not-exist"}, ...
+{"a/../b", c + "/b"}, ...
+{"~", stdlib.homedir()}, {"~/", stdlib.homedir()}, ...
+{"~/..", stdlib.parent(stdlib.homedir())}, ...
+{mfilename("fullpath") + ".m/..", stdlib.parent(mfilename("fullpath"))}, ...
+{"~/not-exist/a/..", stdlib.homedir() + "/not-exist"}
+};
+end
+
+end
+
 methods (Test)
 
-function test_resolve_non_exist(tc)
+function test_resolve_relative(tc, use_java)
 import matlab.unittest.constraints.StartsWithSubstring
 import matlab.unittest.constraints.ContainsSubstring
 
@@ -10,49 +32,34 @@ td = stdlib.posix(pwd());
 
 % all non-existing files
 
-tc.verifyEqual(stdlib.resolve(""), stdlib.posix(td))
+tc.verifyEqual(stdlib.resolve("", true, use_java), stdlib.posix(td))
 
-pabs = stdlib.resolve('2foo');
-pabs2 = stdlib.resolve('4foo');
+pabs = stdlib.resolve('2foo', true, use_java);
+pabs2 = stdlib.resolve('4foo', true, use_java);
 tc.verifyThat(pabs, ~StartsWithSubstring("2"))
 tc.verifyTrue(strncmp(pabs, pabs2, 2))
 
-par1 = stdlib.resolve("../2foo");
+par1 = stdlib.resolve("../2foo", true, use_java);
 tc.verifyNotEmpty(par1)
 tc.verifyThat(par1, ~ContainsSubstring(".."))
 
-par2 = stdlib.resolve("../4foo");
+par2 = stdlib.resolve("../4foo", true, use_java);
 tc.verifyTrue(strncmp(par2, pabs2, 2))
 
-pt1 = stdlib.resolve("bar/../2foo");
+pt1 = stdlib.resolve("bar/../2foo", true, use_java);
 tc.verifyNotEmpty(pt1)
 tc.verifyThat(pt1, ~ContainsSubstring(".."))
 
-va = stdlib.resolve("2foo");
-vb = stdlib.resolve("4foo");
+va = stdlib.resolve("2foo", true, use_java);
+vb = stdlib.resolve("4foo", true, use_java);
 tc.verifyThat(va, ~StartsWithSubstring("2"))
 tc.verifyTrue(strncmp(va, vb, 2))
 
 end
 
-function test_resolve_exist(tc)
-
-td = stdlib.posix(pwd());
-
-r = stdlib.parent(mfilename('fullpath'));
-rp = stdlib.parent(r);
-tc.verifyEqual(stdlib.resolve(rp), stdlib.parent(r))
-
-h = stdlib.homedir();
-tc.verifyEqual(stdlib.resolve("~"), h)
-tc.verifyEqual(stdlib.resolve("~/"), h)
-tc.verifyEqual(stdlib.resolve("~/.."), stdlib.parent(h))
-
-tc.verifyEqual(stdlib.resolve("nobody.txt"), td + "/nobody.txt")
-tc.verifyEqual(stdlib.resolve("../nobody.txt"), stdlib.parent(td) + "/nobody.txt")
-
+function test_resolve_fullpath(tc, p, use_java)
+tc.verifyEqual(stdlib.resolve(p{1}, true, use_java), p{2})
 end
-
 
 end
 
