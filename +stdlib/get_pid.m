dc6afaf583fd1cfbd0c8c4de5eb76ece5f2a526c
scivision <scivision@users.noreply.github.com>
2025-01-07 15:44:22 -0500

    get_pid: use intrinsic function if available

--- +stdlib/get_pid.m
+++ +stdlib/get_pid.m
@@ -2,12 +2,18 @@
 
 function pid = get_pid()
 
-if stdlib.isoctave()
-  pid = getpid();
-else
-  pid = feature("getpid");
+try
+  pid = matlabProcessID;
+catch e
+  if strcmp(e.identifier, "MATLAB:UndefinedFunction")
+    pid = uint64(feature("getpid"));
+  elseif strcmp(e.identifier, "Octave:undefined-function")
+    pid = uint64(getpid());
+  else
+    rethrow(e)
+  end
 end
 
 end
 
-%!assert(get_pid() > 0, "expected positive PID")
+%!assert (get_pid() > 0, "expected positive PID")
