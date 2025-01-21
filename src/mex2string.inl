c6e12dce49ff5c0cdbe6c38417907a8f613d428d
scivision <scivision@users.noreply.github.com>
2025-01-21 15:00:05 -0500

    mex: proximate, relatve: inline code reuse

--- src/mex2string.inl
+++ src/mex2string.inl
@@ -1,4 +1,4 @@
-void matlab_2string(matlab::mex::ArgumentList inputs, std::string* target, std::string* link)
+void matlab_2string(matlab::mex::ArgumentList inputs, std::string* s1, std::string* s2)
   {
     std::shared_ptr<matlab::engine::MATLABEngine> matlabEng = getEngine();
 
@@ -11,10 +11,10 @@ void matlab_2string(matlab::mex::ArgumentList inputs, std::string* target, std::
 
     if ((inputs[0].getType() == matlab::data::ArrayType::MATLAB_STRING && inputs[0].getNumberOfElements() == 1)){
         matlab::data::TypedArray<matlab::data::MATLABString> stringArr = inputs[0];
-        *target = stringArr[0];
+        *s1 = stringArr[0];
     } else if (inputs[0].getType() == matlab::data::ArrayType::CHAR){
         matlab::data::CharArray charArr = inputs[0];
-        *target = std::string(charArr.begin(), charArr.end());
+        *s1 = std::string(charArr.begin(), charArr.end());
     } else {
         matlabEng->feval(u"error", 0,
         std::vector<matlab::data::Array>({ factory.createScalar("Mex: First input must be a scalar string or char vector") }));
@@ -22,10 +22,10 @@ void matlab_2string(matlab::mex::ArgumentList inputs, std::string* target, std::
 
      if ((inputs[1].getType() == matlab::data::ArrayType::MATLAB_STRING && inputs[1].getNumberOfElements() == 1)){
         matlab::data::TypedArray<matlab::data::MATLABString> stringArr = inputs[1];
-        *link = stringArr[0];
+        *s2 = stringArr[0];
     } else if (inputs[1].getType() == matlab::data::ArrayType::CHAR){
         matlab::data::CharArray charArr = inputs[1];
-        *link= std::string(charArr.begin(), charArr.end());
+        *s2 = std::string(charArr.begin(), charArr.end());
     } else {
         matlabEng->feval(u"error", 0,
         std::vector<matlab::data::Array>({ factory.createScalar("Mex: Second input must be a scalar string or char vector") }));
--- src/relative_to.cpp
+++ src/relative_to.cpp
@@ -12,49 +12,23 @@
 #include "ffilesystem.h"
 
 
-
 class MexFunction : public matlab::mex::Function {
+private:
+#include "mex2string.inl"
+
 public:
   void operator()(matlab::mex::ArgumentList outputs, matlab::mex::ArgumentList inputs) {
-// boilerplate engine & ArrayFactory setup
+
     std::shared_ptr<matlab::engine::MATLABEngine> matlabEng = getEngine();
 
     matlab::data::ArrayFactory factory;
-// wrangle inputs
+
     std::string base, other;
 
-    if (inputs.size() != 2) {
-      matlabEng->feval(u"error", 0,
-        std::vector<matlab::data::Array>({ factory.createScalar("Mex: Two inputs required") }));
-    }
+    matlab_2string(inputs, &base, &other);
 
-    if ((inputs[0].getType() == matlab::data::ArrayType::MATLAB_STRING && inputs[0].getNumberOfElements() == 1)){
-        matlab::data::TypedArray<matlab::data::MATLABString> stringArr = inputs[0];
-        base = stringArr[0];
-    } else if (inputs[0].getType() == matlab::data::ArrayType::CHAR){
-        matlab::data::CharArray charArr = inputs[0];
-        base.assign(charArr.begin(), charArr.end());
-    } else {
-        matlabEng->feval(u"error", 0,
-        std::vector<matlab::data::Array>({ factory.createScalar("Mex: First input must be a scalar string or char vector") }));
-    }
-
-     if ((inputs[1].getType() == matlab::data::ArrayType::MATLAB_STRING && inputs[1].getNumberOfElements() == 1)){
-        matlab::data::TypedArray<matlab::data::MATLABString> stringArr = inputs[1];
-        other = stringArr[0];
-    } else if (inputs[1].getType() == matlab::data::ArrayType::CHAR){
-        matlab::data::CharArray charArr = inputs[1];
-        other.assign(charArr.begin(), charArr.end());
-    } else {
-        matlabEng->feval(u"error", 0,
-        std::vector<matlab::data::Array>({ factory.createScalar("Mex: Second input must be a scalar string or char vector") }));
-    }
-
-// actual function algorithm / computation
     std::error_code ec;
-    std::string out;
-
-    out = std::filesystem::relative(other, base, ec).generic_string();
+    std::string out = std::filesystem::relative(other, base, ec).generic_string();
 
     if(ec)
       matlabEng->feval(u"error", 0,
