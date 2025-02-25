38771f4c54a2743f7a013b9960a89471989c5c3e
scivision <scivision@users.noreply.github.com>
2025-02-25 11:52:03 -0500

    doc: relativize

--- +stdlib/relative_to.m
+++ +stdlib/relative_to.m
@@ -8,6 +8,9 @@
 % * rel (1,1) string
 %
 % This function is written in C++ using STL <filesystem>
+%
+% Note: Java Path.relativize has an algorithm so different that we choose not to use it.
+% https://docs.oracle.com/javase/8/docs/api/java/nio/file/Path.html#relativize-java.nio.file.Path-
 
 function relative_to(~,~)
 error("buildtool mex")
