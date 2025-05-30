95f56e2c3371179cf8690e6afb7499668832131e
scivision <scivision@users.noreply.github.com>
2025-05-30 19:00:32 -0400

    filename: efficiency, polymorphic char-string in-out

--- +stdlib/filename.m
+++ +stdlib/filename.m
@@ -7,20 +7,18 @@
 
 function f = filename(p)
 arguments
-  p (1,1) string
+  p {mustBeTextScalar}
 end
 
 % NOT https://docs.oracle.com/en/java/javase/21/docs/api/java.base/java/io/File.html#getName()
 % because by our definition, a trailing directory component is not part of the filename
 % this is like C++17 filesystem::path::filename
 
-p = stdlib.posix(p);
-
-parts = strsplit(p, "/");
+parts = strsplit(stdlib.posix(p), '/');
 
 f = parts{end};
 
-try %#ok<TRYNC>
+if isstring(p)
   f = string(f);
 end
 
