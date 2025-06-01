05be1119aed3a88fbe82c96b97d8c48431d48565
scivision <scivision@users.noreply.github.com>
2025-06-01 01:39:48 -0400

    test:absolute: improve coverage

--- test/TestAbsolute.m
+++ test/TestAbsolute.m
@@ -4,30 +4,16 @@ properties
 td
 end
 
-
 properties (TestParameter)
-p1
-p2
-end
-
-
-methods (TestParameterDefinition, Static)
-
-function p1 = init1arg()
-p1 = {"", "hi", "./hi", "../hi"};
-end
-
-function p2 = init2arg()
-p2 = {{"", ""}, {"", "hi"}, {"hi", ""}, {"there", "hi"}};
-end
-
+p1 = {'', "", "hi", "./hi", "../hi"};
+p2 = {{'', ''}, {'', ""}, {"", ''}, {"", ""}, {"", "hi"}, {"hi", ""}, {'there', "hi"}};
 end
 
 
 methods(TestClassSetup)
 function set_cwd(tc)
 import matlab.unittest.fixtures.CurrentFolderFixture
-tc.td = string(stdlib.posix(tc.createTemporaryFolder()));
+tc.td = stdlib.posix(tc.createTemporaryFolder());
 tc.applyFixture(CurrentFolderFixture(tc.td))
 end
 end
@@ -40,7 +26,11 @@ function test_absolute1arg(tc, p1)
 r = tc.td;
 
 if strlength(p1)
-  r = r + "/" + p1;
+  r = strcat(r, '/', p1);
+end
+
+if isstring(p1)
+  r = string(r);
 end
 
 tc.verifyEqual(stdlib.absolute(p1), r)
@@ -52,11 +42,15 @@ function test_absolute2arg(tc, p2)
 r = tc.td;
 
 if strlength(p2{2})
-  r = r + "/" + p2{2};
+  r = strcat(r, '/', p2{2});
 end
 
 if strlength(p2{1})
-  r = r + "/" + p2{1};
+  r = strcat(r, '/', p2{1});
+end
+
+if isstring(p2{1})
+  r = string(r);
 end
 
 tc.verifyEqual(stdlib.absolute(p2{1}, p2{2}), r)
