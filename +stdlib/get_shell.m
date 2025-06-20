ea80e5b348ea648e21df634f0abdf673e399b68e
scivision <scivision@users.noreply.github.com>
2025-06-20 19:08:50 -0400

    get_shell: current running shell rather than default shell

--- +stdlib/get_shell.m
+++ +stdlib/get_shell.m
@@ -1,10 +1,22 @@
-%% GET_SHELL shell of the current user
-% return value of environment variable SHELL
-% this is mostly relevant on unix-like systems
+%% GET_SHELL full path to currently running shell
 
 function s = get_shell()
 
-s = getenv("SHELL");
+if ispc()
+% https://stackoverflow.com/a/61469226
+  cmd = "(dir 2>&1 *`|echo CMD);&<# rem #>echo ($PSVersionTable).PSEdition";
+else
+% https://askubuntu.com/a/1349538
+  cmd = strcat("lsof -p ", '"$$"', " | grep -m 1 txt | xargs -n 1 | tail -n +9 | tr -d [:space:]");
+end
+
+[r, msg] = system(cmd);
+
+if r == 0
+  s = msg;
+else
+  s = getenv("SHELL");
+end
 
 end
 
