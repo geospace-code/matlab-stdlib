71d861026ef30ad6e4e11c70abb2baa9781525c1
Michael Hirsch <scivision@users.noreply.github.com>
2025-07-21 02:12:02 -0400

    os_version: .net too

--- +stdlib/os_version.m
+++ +stdlib/os_version.m
@@ -7,12 +7,20 @@
 
 function [os, version] = os_version()
 
-os = javaMethod("getProperty", "java.lang.System", "os.name");
-version = javaMethod("getProperty", "java.lang.System", "os.version");
+if stdlib.has_dotnet()
+  v = System.Environment.OSVersion.VersionString;
+  vs = split(string(v), ' ');
+  version = vs(end);
+  os = join(vs(1:end-1));
+else
+  os = javaMethod("getProperty", "java.lang.System", "os.name");
+  version = javaMethod("getProperty", "java.lang.System", "os.version");
+  try  %#ok<*TRYNC>
+    os = string(os);
+    version = string(version);
+  end
+end
 
-try  %#ok<*TRYNC>
-  os = string(os);
-  version = string(version);
 end
 
 %!test
