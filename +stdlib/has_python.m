68ca88a5a1049496a5381319e377b8011b5b09ea
Michael Hirsch <scivision@users.noreply.github.com>
2025-07-21 23:09:45 -0400

    has_python: allowed R2022b+

--- +stdlib/has_python.m
+++ +stdlib/has_python.m
@@ -1,6 +1,15 @@
 %% HAS_PYTHON checks if Python is available in the current environment.
+%
+% https://www.mathworks.com/support/requirements/python-compatibility.html
 
 function y = has_python()
+
+if isMATLABReleaseOlderThan('R2022b')
+  % Matlab this old doesn't work as nice with Python
+  % also, R2022b was the first to support Python 3.10
+  y = false;
+  return
+end
 
 try
   pe = pyenv();
