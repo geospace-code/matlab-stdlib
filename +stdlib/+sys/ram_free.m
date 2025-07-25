13721a4cb9590355f536ca69661fe1e11c7e063f
scivision <scivision@users.noreply.github.com>
2025-07-24 21:24:00 -0400

    sys.ram* more accurate

--- +stdlib/+sys/ram_free.m
+++ +stdlib/+sys/ram_free.m
@@ -5,7 +5,7 @@ bytes = 0;
 if ispc()
   cmd = 'pwsh -c "(Get-CimInstance -ClassName CIM_OperatingSystem).FreePhysicalMemory * 1KB"';
 elseif ismac()
-  cmd = 'sysctl -n hw.memsize';
+  cmd = 'sysctl -n vm.page_free_count';
 else
   cmd = "free -b | awk '/Mem:/ {print $4}'";
 end
@@ -13,6 +13,10 @@ end
 [s, m] = system(cmd);
 if s == 0
   bytes = str2double(m);
+  if ismac()
+    bytes = bytes * 4096; % Assuming a page size of 4096 bytes
+  end
+
 end
 
 bytes = uint64(bytes);
