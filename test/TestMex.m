948eb495b3e6afa972fed5050979060e714c8b74
Michael Hirsch <scivision@users.noreply.github.com>
2025-07-25 12:52:46 -0400

    test: python tag

--- test/TestMex.m
+++ test/TestMex.m
@@ -2,18 +2,6 @@ classdef TestMex < matlab.unittest.TestCase
 
 methods (Test, TestTags = "mex")
 
-function test_is_char_device(tc)
-% /dev/stdin may not be available on CI systems
-if ispc
-  n = "NUL";
-else
-  n = "/dev/null";
-end
-
-tc.verifyTrue(stdlib.is_char_device(n))
-end
-
-
 function test_remove_file(tc)
 tc.assertTrue(stdlib.is_mex_fun("stdlib.remove"))
 
---
+++ test/TestPython.m
@@ -0,0 +1,24 @@
+classdef TestPython < matlab.unittest.TestCase
+
+methods (TestClassSetup)
+function check_python_available(tc)
+tc.assumeTrue(stdlib.has_python(), "Python is not available, skipping tests")
+end
+end
+
+methods (Test, TestTags = "python")
+
+function test_is_char_device(tc)
+% /dev/stdin may not be available on CI systems
+if ispc
+  n = "NUL";
+else
+  n = "/dev/null";
+end
+
+tc.verifyTrue(stdlib.is_char_device(n))
+end
+
+end
+
+end
