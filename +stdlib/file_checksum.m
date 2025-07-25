397444737cc27c5344592b346a26ba75a5a9d7f1
Michael Hirsch <scivision@users.noreply.github.com>
2025-07-24 21:36:33 -0400

    checksum dedupe

--- +stdlib/file_checksum.m
+++ +stdlib/file_checksum.m
@@ -25,31 +25,30 @@ file_chunk = 10e6;  % arbitrary (bytes) didn't seem to be very sensitive for spe
 fid = fopen(file, 'r');
 assert(fid > 1, "could not open file %s", file)
 
-
 if stdlib.has_dotnet()
+
   inst = System.Security.Cryptography.HashAlgorithm.Create(method);
   while ~feof(fid)
     % https://docs.microsoft.com/en-us/dotnet/api/system.security.cryptography.hashalgorithm.computehash
     inst.ComputeHash(fread(fid, file_chunk, '*uint8'));
   end
+  h = uint8(inst.Hash);
+
 elseif stdlib.has_java()
+
   inst = javaMethod("getInstance", "java.security.MessageDigest", method);
   while ~feof(fid)
     % https://docs.oracle.com/en/java/javase/21/docs/api/java.base/java/security/MessageDigest.html#update(byte)
     inst.update(fread(fid, file_chunk, '*uint8'));
   end
+  % https://docs.oracle.com/en/java/javase/21/docs/api/java.base/java/security/MessageDigest.html#digest()
+  h = typecast(inst.digest, 'uint8');
+
 else
   error('no supported hash method found, please install .NET or Java')
 end
 
 fclose(fid);
-
-if stdlib.has_dotnet()
-  h = uint8(inst.Hash);
-elseif stdlib.has_java()
-  % https://docs.oracle.com/en/java/javase/21/docs/api/java.base/java/security/MessageDigest.html#digest()
-  h = typecast(inst.digest, 'uint8');
-end
 
 hash = sprintf('%.2x', h);
 
