54fdb908d83b2d8b801e473609937cfbf644dabe
Michael Hirsch <scivision@users.noreply.github.com>
2025-07-27 17:17:23 -0400

    java:inode: use absolute for Java 1.8 stability

--- +stdlib/+java/inode.m
+++ +stdlib/+java/inode.m
@@ -1,7 +1,12 @@
 function i = inode(p)
 
 opt = javaMethod("values", "java.nio.file.LinkOption");
-i = javaMethod("getAttribute", "java.nio.file.Files", javaPathObject(p), "unix:ino", opt);
+
+jp = javaPathObject(stdlib.absolute(p));
+% Java 1.8 benefits from the absolute() for stability--it's not an issue
+% on every computer.
+
+i = javaMethod("getAttribute", "java.nio.file.Files", jp, "unix:ino", opt);
 
 i = uint64(i);
 end
