5fd8f34ea14139b6717c0ea2ad4ef16ea33e76c9
scivision <scivision@users.noreply.github.com>
2025-07-28 15:14:21 -0400

    qualify upgrade script

--- scripts/MatlabReleaseUpgrade.m
+++ scripts/MatlabReleaseUpgrade.m
@@ -1,6 +1,13 @@
 disp(matlabRelease())
 
+assert(isMATLABReleaseOlderThan('R2025a'), "Matlab R2025a changed the upgrade process, use the GUI.")
+
 r = fullfile(matlabroot, "bin", computer("arch"));
 cmd = fullfile(r, "MathWorksUpdateInstaller");
+if ispc()
+  cmd = cmd + ".exe";
+end
+
+assert(isfile(cmd), "MathWorks Update Installer not found at %s", cmd)
 
 fprintf("Run this command in Terminal to check for Matlab upgrade:\n\n%s\n\n", cmd)
