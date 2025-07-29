1f037a00b1dc4c8480c5dbdc86f7f387b3ff3df8
Michael Hirsch <scivision@users.noreply.github.com>
2025-07-29 17:05:31 -0400

    choose_method for programmatic selection and dedupe

---
+++ +stdlib/private/choose_method.m
@@ -0,0 +1,31 @@
+function fun = choose_method(method, name)
+arguments
+  method (1,:) string 
+  name (1,1) string
+end
+
+if isscalar(method)
+  % bypass checking for about 20% speed boost
+  fun = str2func("stdlib." + method + "." + name);
+  return
+end
+
+for m = method
+  if m == "native"
+    has = true;
+  else
+    has = str2func("stdlib.has_" + m);
+  end
+
+  if has()
+    fun = str2func("stdlib." + m + "." + name);
+    f = functions(fun);
+    if ~isempty(f.file) || isfield(f, 'opaqueType')
+      return
+    end
+  end
+end
+
+error("Could not find %s using %s", name, method)
+
+end
