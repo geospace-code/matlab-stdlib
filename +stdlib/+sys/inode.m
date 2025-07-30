506d7bf2b39ad3150e71d7fb389643e56c77ce66
scivision <scivision@users.noreply.github.com>
2025-07-30 16:59:52 -0400

    sys.inode: correct Windows

--- +stdlib/+sys/inode.m
+++ +stdlib/+sys/inode.m
@@ -1,8 +1,11 @@
+%% SYS.INODE get file inode
+
 function i = inode(m)
 
 i = 0;
 
 if ispc()
+  % https://learn.microsoft.com/en-us/windows-server/administration/windows-commands/fsutil-file
   cmd = "fsutil file queryfileid " + m;
 elseif ismac()
   cmd = "stat -f %i " + m;
@@ -12,7 +15,17 @@ end
 
 [s, m] = system(cmd);
 if s == 0
-  i = str2double(m);
+  if ispc()
+    m = strrep(strip(m), "File ID is ", "");
+    % https://www.mathworks.com/help/releases/R2025a/matlab/ref/hex2dec.html
+    i = sscanf(m, '%lx', 1);
+    if i == intmax('uint64')
+      L = strlength(m);
+      i = [sscanf(m(1:L-1), '%lx', 1), sscanf(m(L:end), '%lx', 1)];
+    end
+  else
+    i = str2double(m);
+  end
 end
 
 i = uint64(i);
