a66d336e8a29276c3ee7173badfbc5d9a84f315e
Michael Hirsch <scivision@users.noreply.github.com>
2025-07-30 23:51:43 -0400

    doc

--- +stdlib/is_char_device.m
+++ +stdlib/is_char_device.m
@@ -2,6 +2,14 @@
 %
 % e.g. NUL, /dev/null
 % false if file does not exist
+%
+% Windows: Console handles
+%   CONIN$, CONOUT$, STDIN, STDOUT, STDERR are not detected. Only NUL is detected.
+%   in our C-API-based Ffilesystem library, we use GetFileType() with CreateFile() to
+%   detect those handles successfully, but they don't work here.
+%
+% Ref: https://learn.microsoft.com/en-us/windows/console/console-handles
+
 
 function ok = is_char_device(file, method)
 arguments
