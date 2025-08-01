5e39d4c1702828a1b2407cb99d3bac5fca542861
scivision <scivision@users.noreply.github.com>
2025-08-01 15:06:08 -0400

    add checkout_license()

---
+++ +stdlib/checkout_license.m
@@ -0,0 +1,36 @@
+%% CHECKOUT_LICENSE checkout Matlab license
+% NOTE: checking out a license holds that license as long as Matlab is
+% running. This could impact other users at an institution with a limited
+% number of networked licenses. Only use this function at point-of-use,
+% that is, when it's certain the function next called requires this license
+% to be checked out.
+%
+% This function provides a more accurate indicator that a function can
+% be used rather than just checking if the relevant toolbox is installed.
+
+function [ok, featureName] = checkout_license(packageName)
+arguments
+  packageName {mustBeTextScalar}
+end
+
+ok = false;
+
+addons = matlab.addons.installedAddons;
+installedPackages = addons.Name;
+name = addons.Name(strcmpi(installedPackages, packageName));
+
+if isempty(name)
+  warning('stdlib:checkout_license:ValueError', 'Did not find an installed package %s.\nInstalled packages:\n%s', packageName, join(installedPackages, newline))
+  return
+end
+
+if ~usejava('jvm')
+  error('Java must be enabled for this function to work.')
+end
+
+% https://www.mathworks.com/help/matlab/matlab_env/index-of-code-analyzer-checks.html
+featureName = string(com.mathworks.product.util.ProductIdentifier.get(name).getFlexName()); %#ok<JAPIMATHWORKS>
+
+ok = license('checkout', featureName);
+
+end
