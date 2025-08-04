487fd9d896438976790b637addd0d258755db6af
Michael Hirsch <scivision@users.noreply.github.com>
2025-08-04 03:38:07 -0400

    filesystem_type: handle spaces

--- +stdlib/+sys/filesystem_type.m
+++ +stdlib/+sys/filesystem_type.m
@@ -6,11 +6,11 @@ if ~stdlib.exists(p), return, end
 
 if ispc()
   dl = extractBefore(stdlib.absolute(p), 2);
-  cmd = "pwsh -c (Get-Volume -DriveLetter " + dl + ").FileSystem";
+  cmd = sprintf('pwsh -c (Get-Volume -DriveLetter "%s").FileSystem', dl);
 elseif ismac()
-  cmd = "df -aHY " + p + " | awk 'NR==2 {print $2}'";
+  cmd = sprintf('df -aHY "%s" | awk ''NR==2 {print $2}''', p);
 else
-  cmd = "df --output=fstype " + p + " | tail -n 1";
+  cmd = sprintf('df --output=fstype "%s" | tail -n 1', p);
 end
 
 [s, t] = system(cmd);
