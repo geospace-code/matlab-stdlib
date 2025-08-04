e3bf3e34bec042b7021ac6c8b405b5a8c1d79c62
Michael Hirsch <scivision@users.noreply.github.com>
2025-08-04 12:57:22 -0400

    ci: codeIssues >= R2023a

--- +stdlib/+legacy/private/file_attributes.m
+++ +stdlib/+legacy/private/file_attributes.m
@@ -7,7 +7,7 @@ assert(~stdlib.strempty(p), 'Path must not be empty.')
 assert(status == 1, "'%s' is not a file or directory.", p);
 
 a = s;
-for n = {"GroupRead", "GroupWrite", "GroupExecute", "OtherRead", "OtherWrite", "OtherExecute"}
+for n = ["GroupRead", "GroupWrite", "GroupExecute", "OtherRead", "OtherWrite", "OtherExecute"]
   name = n{1};
 
   if ~isfield(a, name) || isnan(a.(name))
