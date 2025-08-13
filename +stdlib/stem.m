2694232558eb1fe7ad6917452c4a95b3a2683ca6
Michael Hirsch <scivision@users.noreply.github.com>
2025-08-13 00:06:14 -0400

    stem: 100x speedup

--- +stdlib/stem.m
+++ +stdlib/stem.m
@@ -1,26 +1,22 @@
 %% STEM base file name without directory or suffix
-% STEM Extracts the file name without directory or suffix from a path.
 %   s = stem(p) returns the stem (base name) of the file specified by the path p.
 %   leading dot filenames are allowed.
 %
-%   Input:
-%     p - Character vector or string scalar specifying the file path.
+%%% Inputs:
+% * p: Character vector or string scalar specifying the file path.
 %
-%   Output:
-%     s - Character vector or string scalar containing the file name without directory or suffix.
-
-function s = stem(p)
+%%% Output:
+% * s: Character vector or string scalar containing the file name without directory or suffix.
+%
+% Note: fileparts() was about 100x faster then using multiple steps with pattern.
+function s = stem(filepath)
 arguments
-  p string
+  filepath string
 end
 
-p0 = asManyOfPattern(wildcardPattern + ("/" | filesep));
-% p1 matches a file extension (e.g., '.txt') or the end of the string
-p1 = ("." + alphanumericsPattern + textBoundary('end')) | textBoundary('end');
-
-s = extractBetween(p, p0, p1);
+[~, s, e] = fileparts(filepath);
 
 i = stdlib.strempty(s);
-s(i) = extractAfter(p(i), p0);
+s(i) = e(i);
 
 end
