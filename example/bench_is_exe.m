ded57916abf6a65a50e87481d2772ca86dad71ca
scivision <scivision@users.noreply.github.com>
2025-08-14 10:48:33 -0400

    update is_exe benchmark

--- example/bench_is_exe.m
+++ example/bench_is_exe.m
@@ -1,12 +1,22 @@
-%% benchmark
+function bench_is_exe()
 
-f = mfilename("fullpath") + ".m";
-addpath(fullfile(fileparts(f), ".."))
+in = mfilename("fullpath") + ".m";
+r = fileparts(fileparts(in));
+addpath(r)
+obj = onCleanup(@() rmpath(r));
 
-%f = tempname;
+ff = @() stdlib.is_exe(in);
+fn = @() stdlib.native.is_exe(in);
+fl = @() stdlib.legacy.is_exe(in);
+fj = @() stdlib.java.is_exe(in);
+fp = @() stdlib.python.is_exe(in);
 
-fno = @() stdlib.is_exe(f);
+tj = timeit(fj) * 1e3;
+tp = timeit(fp) * 1e3;
+tf = timeit(ff) * 1e3;
+tn = timeit(fn) * 1e3;
+tl = timeit(fl) * 1e3;
 
-t_no = timeit(fno);
+fprintf('Full: %f\nNative: %f\nLegacy: %f\nJava: %f\nPython: %f\n', tf, tl, tn, tj, tp);
 
-disp("No Java: " + t_no + " s")
+end
