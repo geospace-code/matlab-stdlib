c8ad266666f4da6893a462d225f54042cbb6960d
scivision <scivision@users.noreply.github.com>
2025-09-02 07:55:13 -0400

    perl backend: more reliable fullpath to Perl

--- +stdlib/private/executable.pl
+++ +stdlib/private/executable.pl
@@ -1,1 +1,11 @@
-print $^X;
+# https://perldoc.perl.org/variables/$%5EX
+# $^X may be relative, so we need to resolve it to an absolute path
+# we do not use $Config{perlpath} as that's a build-time variable and is thus
+# incorrect when a buildbot was used e.g. Windows Perl from Matlab
+#
+# https://perldoc.perl.org/Cwd#abs_path
+# abs_path is effectively realpath(3)
+
+use Cwd 'abs_path';
+
+print abs_path($^X);
