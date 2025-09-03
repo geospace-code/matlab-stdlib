699d4ff7bd510c40c48772886879ca3905ccd5cd
scivision <scivision@users.noreply.github.com>
2025-09-02 21:18:43 -0400

    .dotnet.hostname: get network name like other backends [skip ci]

--- +stdlib/+dotnet/hostname.m
+++ +stdlib/+dotnet/hostname.m
@@ -1,9 +1,11 @@
 %% DOTNET.GET_HOSTNAME get the computer network hostname
-% not necessary FQDN
 
 function n = hostname()
 
-n = char(System.Environment.MachineName);
+%n = char(System.Environment.MachineName);
 % https://learn.microsoft.com/en-us/dotnet/api/system.environment.machinename
+
+n = char(System.Net.Dns.GetHostName());
+% https://learn.microsoft.com/en-us/dotnet/api/system.net.dns.gethostname
 
 end
