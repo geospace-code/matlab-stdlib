af616868e9304a1e8e3b1b72c69a03b116aa0ddf
scivision <scivision@users.noreply.github.com>
2025-09-08 12:17:46 -0400

    python: catch missing import

--- +stdlib/+python/private/pythonException.m
+++ +stdlib/+python/private/pythonException.m
@@ -1,8 +1,8 @@
 function pythonException(e)
 
 switch e.identifier
-  case 'MATLAB:Python:PythonUnavailable'
-    % pass
+  case {'MATLAB:Python:PythonUnavailable', 'MATLAB:undefinedVarOrClass'}
+    % Python not available or Python imported module not available
   case 'MATLAB:Python:PyException'
     if ~contains(e.message, ["is not in the subpath of", "NotADirectoryError", "FileNotFoundError"])
       rethrow(e)
