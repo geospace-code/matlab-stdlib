4f453e10f52a58cf791ad0aaed1d897c54fae499
scivision <scivision@users.noreply.github.com>
2025-09-11 11:29:46 -0400

    testSame: simpler

--- +stdlib/+python/samepath.m
+++ +stdlib/+python/samepath.m
@@ -1,12 +1,11 @@
 function y = samepath(path1, path2)
 
-if ~stdlib.exists(path1) || ~stdlib.exists(path2)
-  y = false;
-  return
-end
-  
 try
-  y = py.os.path.samefile(path1, path2);
+ if py.os.path.exists(path1) && py.os.path.exists(path2)
+    y = py.os.path.samefile(path1, path2);
+ else
+    y = false;
+ end
 catch e
   pythonException(e)
   y = logical.empty;
--- test/TestSame.m
+++ test/TestSame.m
@@ -1,5 +1,5 @@
 classdef (SharedTestFixtures={ matlab.unittest.fixtures.PathFixture(fileparts(fileparts(mfilename('fullpath'))))}, ...
-          TestTags = {'impure'}) ...
+          TestTags = {'R2019b', 'impure'}) ...
     TestSame < matlab.unittest.TestCase
 
 properties (TestParameter)
@@ -9,29 +9,38 @@ p_same = {...
 {"..", pwd() + "/.."}, ...
 {pwd(), pwd() + "/."}}
 
-backend
-end
-
-methods (TestParameterDefinition, Static)
-function backend = setupBackends()
-backend = init_backend("samepath");
-end
+backend = {'python', 'java', 'perl', 'sys', 'native'}
 end
 
 
-methods(Test, TestTags={'R2021a'})
+methods(Test)
 
 function test_samepath(tc, p_same, backend)
-r = stdlib.samepath(p_same{:}, backend);
+[r, b] = stdlib.samepath(p_same{:}, backend);
+tc.assertEqual(char(b), backend)
 tc.verifyClass(r, 'logical')
-tc.verifyTrue(r)
+
+if ismember(backend, stdlib.Backend().select('samepath'))
+  tc.verifyTrue(r)
+else
+  tc.assertEmpty(r)
+end
 end
 
 
 function test_samepath_notexist(tc, backend)
+
 t = tempname();
-tc.verifyFalse(stdlib.samepath("", "", backend))
-tc.verifyFalse(stdlib.samepath(t, t, backend))
+r1 = stdlib.samepath("", "", backend);
+r2 = stdlib.samepath(t, t, backend);
+
+if ismember(backend, stdlib.Backend().select('samepath'))
+  tc.verifyFalse(r1)
+  tc.verifyFalse(r2)
+else
+  tc.assertEmpty(r1)
+  tc.assertEmpty(r2)
+end
 end
 
 end
