f8d8a064725bf561315d6f1d6face294a568980e
scivision <scivision@users.noreply.github.com>
2025-09-11 14:33:55 -0400

    TestTime < R2021a

--- test/TestTime.m
+++ test/TestTime.m
@@ -1,17 +1,11 @@
 classdef (SharedTestFixtures={ matlab.unittest.fixtures.PathFixture(fileparts(fileparts(mfilename('fullpath'))))}, ...
-          TestTags = {'R2021a', 'impure'}) ...
+          TestTags = {'R2019b', 'impure'}) ...
     TestTime < matlab.unittest.TestCase
 
 properties (TestParameter)
-B_set_modtime
+B_set_modtime = {'java', 'python', 'sys'}
 end
 
-
-methods (TestParameterDefinition, Static)
-function B_set_modtime = setupBackends()
-B_set_modtime = init_backend("set_modtime");
-end
-end
 
 methods(TestClassSetup)
 function test_path(tc)
@@ -36,10 +30,13 @@ t0 = stdlib.get_modtime(fn);
 
 ok = stdlib.set_modtime(fn, datetime("now"), B_set_modtime);
 
-tc.assertTrue(ok)
-t1 = stdlib.get_modtime(fn);
-
-tc.verifyGreaterThanOrEqual(t1, t0)
+if ismember(B_set_modtime, stdlib.Backend().select('set_modtime'))
+  tc.assertTrue(ok)
+  t1 = stdlib.get_modtime(fn);
+  tc.verifyGreaterThanOrEqual(t1, t0)
+else
+  tc.assertEmpty(ok)
+end
 end
 
 end
