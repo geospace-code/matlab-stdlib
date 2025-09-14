553a325e78e4220575137fbb8c3dcedadd0318af
scivision <scivision@users.noreply.github.com>
2025-09-14 13:23:18 -0400

    python: catch NotImplementedError

--- +stdlib/+python/private/pythonException.m
+++ +stdlib/+python/private/pythonException.m
@@ -4,7 +4,7 @@ switch e.identifier
   case {'MATLAB:Python:PythonUnavailable', 'MATLAB:undefinedVarOrClass'}
     % Python not available or Python imported module not available
   case 'MATLAB:Python:PyException'
-    if ~contains(e.message, ["is not in the subpath of", "NotADirectoryError", "FileNotFoundError"])
+    if ~contains(e.message, ["NotImplementedError", "is not in the subpath of", "NotADirectoryError", "FileNotFoundError"])
       rethrow(e)
     end
   otherwise, rethrow(e)
