cce54b169cab7e2fcc0bf86fab7a7c34358b656f
scivision <scivision@users.noreply.github.com>
2025-09-16 19:09:36 -0400

    is_mount: correct shell syntax

--- +stdlib/+sys/is_mount.m
+++ +stdlib/+sys/is_mount.m
@@ -7,18 +7,21 @@ if ~stdlib.exists(filepath)
 end
 
 if ispc()
-  if ismember(filepath, ["/", "\"]) || ...
-      (endsWith(filepath, ["/", filesep]) && isfolder(filepath) && filepath == stdlib.root(filepath))
+  fs = {'/', filesep};
+  if ismember(filepath, fs) || ...
+      (endsWith(filepath, fs) && ...
+       isfolder(filepath) && ...
+       strcmp(filepath, stdlib.root(filepath)))
     y = true;
     return
   end
   cmd = sprintf('pwsh -c "(Get-Item -Path ''%s'').Attributes.ToString().Contains(''ReparsePoint'')"', filepath);
 elseif ismac()
-  if filepath == "/"
+  if strcmp(filepath, '/')
     y = true;
     return
   end
-  cmd = sprintf('[ $(stat -f %%d "%s") != $(stat -f %%d "%s")]', ...
+  cmd = sprintf('[ $(stat -f %%d "%s") != $(stat -f %%d "%s") ]', ...
                 filepath, stdlib.parent(filepath));
 else
   cmd = sprintf('mountpoint -q "%s"', filepath);
@@ -27,7 +30,7 @@ end
 [s, m] = system(cmd);
 
 if ispc()
-  y = s == 0 && m == "True";
+  y = s == 0 && strcmp(m, 'True');
 else
   y = s == 0;
 end
