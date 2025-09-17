3288e25cc6ed0f709338394dd04c90ea28988760
scivision <scivision@users.noreply.github.com>
2025-09-17 16:19:25 -0400

    speed, compatibility

--- +stdlib/private/file_attributes.m
+++ +stdlib/private/file_attributes.m
@@ -7,12 +7,13 @@ if status ~= 1
   return
 end
 
-for n = ["GroupRead", "GroupWrite", "GroupExecute", "OtherRead", "OtherWrite", "OtherExecute"]
+pv = {'GroupRead', 'GroupWrite', 'GroupExecute', 'OtherRead', 'OtherWrite', 'OtherExecute'};
 
+for i = 1:numel(pv)
+  n = pv{i};
   if ~isfield(a, n) || isnan(a.(n))
     a.(n) = false;
   end
-
 end
 
 end
