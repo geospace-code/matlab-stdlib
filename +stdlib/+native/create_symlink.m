6023b827a11ca0465e8c2d61f2a36b90d7c5cbeb
SciVision <scivision@users.noreply.github.com>
2026-02-16 15:47:44 -0500

    create_symlink: give hint if Windows Developer Mode needed

--- +stdlib/+native/create_symlink.m
+++ +stdlib/+native/create_symlink.m
@@ -5,7 +5,14 @@ try
   i = true;
 catch e
   switch e.identifier
-    case {'MATLAB:UndefinedFunction', 'MATLAB:io:filesystem:symlink:NeedsAdminPerms'}
+    case {'MATLAB:io:filesystem:symlink:NeedsAdminPerms'}
+      if ispc
+        error('MATLAB:io:filesystem:symlink:NeedsAdminPerms', ...
+            'Consider enabling Windows Developer Mode to allow non-priviliged symlinks https://learn.microsoft.com/en-us/windows/advanced-settings/developer-mode#enable-developer-mode')
+      else
+        rethrow(e)
+      end
+    case {'MATLAB:UndefinedFunction'}
       i = logical([]);
     case {'MATLAB:io:filesystem:symlink:FileExists', 'MATLAB:io:filesystem:symlink:TargetNotFound'}
       i = false;
