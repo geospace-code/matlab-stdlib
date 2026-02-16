10e544ffd7b338095c49073f88be92c821396c62
SciVision <scivision@users.noreply.github.com>
2026-02-16 17:48:38 -0500

    python.inode: correct type casting like python.device
    Disregard older Matlab <= R2022a as they can use other methods, Python doesn't work as well
    with those old Matlab versions anyway.

--- +stdlib/+python/inode.m
+++ +stdlib/+python/inode.m
@@ -1,12 +1,14 @@
 function i = inode(file)
 
 try
-  i = int64(py.os.stat(file).st_ino);
-  % int64 first is for Matlab <= R2022a
+  i = py.os.stat(file).st_ino;
+  % Matlab <= R2022a wants int64 before uint64, but this can make OverflowError on Windows within Python
+  % because on Windows st_dev is a 64-bit unsigned integer
 catch e
   pythonException(e)
   i = [];
 end
 
 i = uint64(i);
+
 end
--- +stdlib/+python/private/disk_usage.m
+++ +stdlib/+python/private/disk_usage.m
@@ -7,9 +7,6 @@ try
     case 'free', i = u.free;
     otherwise, error('stdlib:python:disk_usage:valueError', 'unknown disk_usage property %s', v)
   end
-
-  i = int64(i);
-  % int64 first is for Matlab <= R2022a
 catch
   i = [];
 end
