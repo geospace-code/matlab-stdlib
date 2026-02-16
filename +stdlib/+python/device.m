b9fcbe0f69160589230172996fcdac3763e18ee9
SciVision <scivision@users.noreply.github.com>
2026-02-16 16:59:57 -0500

    stdlib.python.device: uint64 to avoid Python OverflowError

--- +stdlib/+python/device.m
+++ +stdlib/+python/device.m
@@ -1,8 +1,9 @@
 function i = device(file)
 
 try
-  i = int64(py.os.stat(file).st_dev);
-  % int64 first is for Matlab <= R2022a
+  i = py.os.stat(file).st_dev;
+  % Matlab <= R2022a wants int64 before uint64, but this can make OverflowError on Windows within Python
+  % because on Windows st_dev is a 64-bit unsigned integer
 catch e
   pythonException(e)
   i = [];
