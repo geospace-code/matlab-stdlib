d13ff7d5857045fa890779d4e8addcfd4b2bed1c
scivision <scivision@users.noreply.github.com>
2026-03-11 02:13:34 -0400

    script matlab python

--- scripts/test_matlab_versions.py
+++ scripts/test_matlab_versions.py
@@ -4,41 +4,42 @@ import platform
 
 # these are HPC system specific
 version_keys = [
-    "2025b",
-    "2025a",
-    "2024b",
-    "2024a",
-    "2023b",
-    "2023a",
-    "2022b",
-    "2022a",
-    "2021b",
-    "2021a",
-    "2020b",
-    "2020a",
-    "2019b",
-    "2019a",
-    "2018b",
-    "2018a",
-    "2017b",
-    "2017a",
+"2025b",
+"2025a",
+"2024b",
+"2024a",
+"2023b",
+"2023a",
+"2022b",
+"2022a",
+"2021b",
+"2021a",
+"2020b",
+"2020a",
+"2019b",
+"2019a",
+"2018b",
+"2018a",
+"2017b",
+"2017a",
 ]
 
 
-@pytest.mark.skipif(not platform.system() == "Linux")
+@pytest.mark.skipif(not platform.system() == "Linux", reason="HPC only")
 @pytest.mark.parametrize("version", version_keys)
 def test_matlab_version(version):
-    print(f"Testing MATLAB version: {version}")
 
-    ret = subprocess.run(["module", "load", f"matlab/{version}"])
+    ret = subprocess.run(f"module is-avail matlab/{version}", shell=True, capture_output=True, text=True)
     if ret.returncode != 0:
-        pytest.skip(f"MATLAB version {version} not available")
+        pytest.skip(f"MATLAB version {version} not available {ret.stderr}")
+
+    lcmd = f"module load matlab/{version} && "
 
     if version >= "2022b":
-        cmd = ["matlab", "-batch", "buildtool test"]
+        cmd = lcmd + "matlab -batch 'buildtool test'"
     elif version >= "2019a":
-        cmd = ["matlab", "-batch", "test_main"]
+        cmd = lcmd + "matlab -batch test_main"
     else:
-        cmd = ["matlab", "-r", "test_main; exit"]
+        cmd = lcmd + "matlab -r 'test_main; exit'"
 
-    subprocess.check_call(cmd)
+    subprocess.check_call(cmd, shell=True)
