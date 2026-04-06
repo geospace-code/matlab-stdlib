24c37265c0522143e0185aab15e12263fa6f5553
scivision <scivision@users.noreply.github.com>
2026-04-05 22:51:52 -0400

    has_python: allow user to disable persistently

--- +stdlib/has_python.m
+++ +stdlib/has_python.m
@@ -3,6 +3,16 @@
 % https://www.mathworks.com/support/requirements/python-compatibility.html
 
 function y = has_python(enable_check)
+% arguments
+%   enable_check (1,1) logical = ~stdlib.matlabOlderThan('R2022a');
+% end
+
+persistent py_enable
+
+if ~isempty(py_enable) && nargin == 0
+  y = py_enable;
+  return
+end
 
 y = false;
 
@@ -13,6 +23,8 @@ end
 if enable_check
   y = ~isempty(stdlib.python_version());
 end
+
+py_enable = y;
 
 end
 
