b196fd50c88ed3becbe2b5cfab80f171bd67fcd5
scivision <scivision@users.noreply.github.com>
2026-06-23 10:43:15 -0500

    example
    example closer to code

--- example/+native/is_writable.m
+++ example/+native/is_writable.m
@@ -8,16 +8,6 @@ if isunix
   props = [props, "GroupWrite", "OtherWrite"];
 end
 
-try
-  y = getPermissions(filePermissions(file), props);
-catch e
-  switch e.identifier
-    case 'MATLAB:io:filesystem:filePermissions:CannotFindLocation'
-      y = logical([]);
-    otherwise
-      rethrow(e)
-  end
-end
-
+y = getPermissions(filePermissions(file), props);
 
 end
--- example/+perl/device.m
+++ example/+perl/device.m
@@ -1,8 +1,8 @@
 %% PERL.DEVICE filesystem device index of path
 
-function [r, cmd] = device(file)
+function [i, cmd] = device(file)
 
-r = uint64([]);
+i = missing;
 
 exe = stdlib.perl_exe();
 if stdlib.strempty(exe)
@@ -13,13 +13,9 @@ c = stdlib.perl.perl2cmd('($f=shift) && -e $f or exit 1; print +(stat $f)[0]');
 
 cmd = sprintf('"%s" -e %s "%s"', exe, c, file);
 
-[s, r] = system(cmd);
+[s, i] = system(cmd);
 if s == 0
-  r = str2double(r);
-else
-  r = [];
+  i = uint64(str2double(i));
 end
-
-r = uint64(r);
 
 end
