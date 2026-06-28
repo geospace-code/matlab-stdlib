73d59aa96f1cb038f1ae0f571ea43528d91f9282
scivision <scivision@users.noreply.github.com>
2026-06-28 17:31:00 -0400

    os_version: validators

--- +stdlib/+java/os_version.m
+++ +stdlib/+java/os_version.m
@@ -1,10 +1,10 @@
 function [os, vers] = os_version()
 
-try
+if stdlib.has_java()
   os = char(javaMethod('getProperty', 'java.lang.System', 'os.name'));
   vers = char(javaMethod('getProperty', 'java.lang.System', 'os.version'));
-catch e
-  os = javaException(e);
+else
+  os = missing;
   vers = missing;
 end
 
