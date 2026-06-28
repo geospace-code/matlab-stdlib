bf25165f03ae49940ac4a0b424aab89b765686e0
scivision <scivision@users.noreply.github.com>
2026-06-28 17:28:13 -0400

    get_process_priority: validators

--- +stdlib/get_process_priority.m
+++ +stdlib/get_process_priority.m
@@ -11,24 +11,12 @@
 
 function [i, b] = get_process_priority(backend)
 arguments
-  backend (1,:) string = ["dotnet", "python", "shell"]
+  backend (1,:) string {mustBeNonempty} = ["dotnet", "python", "shell"]
 end
 
-i = missing;
-
 for b = backend
-  switch b
-    case 'dotnet'
-      i = stdlib.dotnet.get_process_priority();
-    case 'python'
-      if stdlib.has_python()
-        i = stdlib.python.get_process_priority();
-      end
-    case 'shell'
-      i = stdlib.shell.get_process_priority();
-    otherwise
-      error('stdlib:get_process_priority:ValueError', 'Unknown backend: %s', b)
-  end
+  f = str2func("stdlib." + b + ".get_process_priority");
+  i = f();
 
   if ~ismissing(i)
     return
