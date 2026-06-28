7167b019f12787e89ae9844057ae7beb3e8637a3
scivision <scivision@users.noreply.github.com>
2026-06-28 14:51:01 -0400

    java.disk* consolidate

--- +stdlib/+java/disk_available.m
+++ +stdlib/+java/disk_available.m
@@ -1,16 +1,5 @@
 function i = disk_available(file)
 
-try
-  o = javaObject('java.io.File', file);
-  i = javaMethod('getUsableSpace', o);
-  if i < 1
-    i = missing;
-  else
-    i = uint64(i);
-  end
-catch e
-  i = javaException(e);
-end
-
+i = disk_usage(file, 'available');
 
 end
