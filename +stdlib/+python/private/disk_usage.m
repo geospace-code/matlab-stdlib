061548c8425e8a1fd22b5d7444dc7b695d14d38d
scivision <scivision@users.noreply.github.com>
2026-06-28 14:51:24 -0400

    dotnet disk* simplify

--- +stdlib/+python/private/disk_usage.m
+++ +stdlib/+python/private/disk_usage.m
@@ -1,5 +1,8 @@
 function i = disk_usage(file, v)
 
+i = missing;
+
+if stdlib.has_python()
 try
   u = py.shutil.disk_usage(file);
   switch v
@@ -8,8 +11,10 @@ try
     otherwise, error('stdlib:python:disk_usage:valueError', 'unknown disk_usage property %s', v)
   end
   i = uint64(i);
-catch
-  i = missing;
+catch e
+  if e.identifier ~= "MATLAB:undefinedVarOrClass"
+    rethrow(e);
+  end
 end
 
 
