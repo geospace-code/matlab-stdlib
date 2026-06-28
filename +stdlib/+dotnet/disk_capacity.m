dd5946d2cce9d0f975c829a8e399aeb88f110613
scivision <scivision@users.noreply.github.com>
2026-06-28 14:56:21 -0400

    dotnet disk* simplify

--- +stdlib/+dotnet/disk_capacity.m
+++ +stdlib/+dotnet/disk_capacity.m
@@ -2,14 +2,6 @@
 
 function i = disk_capacity(file)
 
-% Windows needs exists() not just strempty()
-if stdlib.has_dotnet() && stdlib.exists(file)
-  % absolutizing is necessary for Windows especially
-  i = System.IO.DriveInfo(System.IO.Path.GetFullPath(file)).TotalSize();
-  % https://learn.microsoft.com/en-us/dotnet/api/system.io.driveinfo.totalsize
-  i = uint64(i);
-else
-  i = missing;
-end
+i = disk_usage(file, 'capacity');
 
 end
