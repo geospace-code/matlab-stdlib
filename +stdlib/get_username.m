213d04d168c4f69ee07071a6baba308536217023
scivision <scivision@users.noreply.github.com>
2026-06-28 17:04:38 -0400

    get_username: validators

--- +stdlib/get_username.m
+++ +stdlib/get_username.m
@@ -8,26 +8,12 @@
 
 function [r, b] = get_username(backend)
 arguments
-  backend (1,:) string = ["java", "dotnet", "python", "shell"]
+  backend (1,:) string {mustBeNonempty} = ["java", "dotnet", "python", "shell"]
 end
 
-r = missing;
-
 for b = backend
-  switch b
-    case 'java'
-      r = stdlib.java.get_username();
-    case 'dotnet'
-      r = stdlib.dotnet.get_username();
-    case 'python'
-      if stdlib.has_python()
-        r = stdlib.python.get_username();
-      end
-    case 'shell'
-      r = stdlib.shell.get_username();
-    otherwise
-      error('stdlib:get_username:ValueError', 'Unknown backend: %s', b)
-  end
+  f = str2func("stdlib." + b + ".get_username");
+  r = f();
 
   if ~ismissing(r)
     return
